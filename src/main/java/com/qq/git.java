701e56f7287fcab94156714ae186f1b0b8a29677
pppc <pppc@qq.com>
2022-05-22 14:42:30 +0800

    first commit

---
+++ src/main/java/com/qq/git.java
@@ -0,0 +1,13 @@
+package com.qq;
+
+/**
+ * @author yqq
+ * @Description
+ * @create 2022-05-22 下午 14:33
+ */
+
+public class git {
+    public static void main(String[] args) {
+        System.out.println("\"hellogit\" = " + "hellogit");
+    }
+}
