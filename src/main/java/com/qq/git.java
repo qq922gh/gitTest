de139129bbf011ac717184ad8aebf8c63f77e981
pppc <pppc@qq.com>
2022-05-22 15:04:07 +0800

    third commit

--- src/main/java/com/qq/git.java
+++ src/main/java/com/qq/git.java
@@ -10,5 +10,6 @@ public class git {
     public static void main(String[] args) {
         System.out.println("\"hellogit\" = " + "hellogit2");
         System.out.println("\"hellogit\" = " + "hotfix");
+        System.out.println("\"hellogit\" = " + "master");
     }
 }
